87e4556d56d5508d85e8638a8de5871e2814e54d
Martin Menegoz <martin.menegoz@univ-grenoble-alpes.fr>
2022-10-19 09:15:51 +0200

    update download script

--- download_cru_data.sh
+++ download_cru_data.sh
@@ -5,7 +5,7 @@
 
 wget -U Mozilla/5.0 'https://filesender.renater.fr/download.php?token=1f0c251e-096f-4038-b5c0-4f3a7b10f46c&files_ids=18593157' -O HMA_cru_ts4.06.1901.2021.tmp.dat.nc.gz
 gunzip HMA_cru_ts4.06.1901.2021.tmp.dat.nc.gz
-wget -U Mozilla/5.0 'https://filesender.renater.fr/?s=download&token=c274a574-1116-44d7-878d-b74e33258f3d' -O elev.0.25-deg.nc
+wget -U Mozilla/5.0 'https://filesender.renater.fr/download.php?token=c274a574-1116-44d7-878d-b74e33258f3d&files_ids=18593388' -O elev.0.25-deg.nc
 mkdir -p data
 mv elev.0.25-deg.nc data/.
 mv HMA_cru_ts4.06.1901.2021.tmp.dat.nc data/.
