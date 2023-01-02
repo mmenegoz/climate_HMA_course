12f6ea54c6b7a04eeb023b18669493da0e8c8415
Martin Ménégoz <37449527+mmenegoz@users.noreply.github.com>
2023-01-02 09:24:34 +0100

    Update download_cru_data.sh

--- download_cru_data.sh
+++ download_cru_data.sh
@@ -4,7 +4,7 @@
 # See https://www.forbesconrad.com/blog/download-wetransfer-to-linux-server-wget/
 
 wget -U Mozilla/5.0 'https://filesender.renater.fr/download.php?token=0c9741dd-cb6f-4a4e-8bae-37ca24a93f09&files_ids=20570999' -O HMA_cru_ts4.06.1901.2021.tmp.dat.nc.gz
-wget -U Mozilla/5.0 'https://filesender.renater.fr/download.php?token=c274a574-1116-44d7-878d-b74e33258f3d&files_ids=18593388' -O elev.0.25-deg.nc
+wget -U Mozilla/5.0 'https://filesender.renater.fr/download.php?token=863294df-3c3f-40d8-945a-55400710e8dc&files_ids=20570949' -O elev.0.25-deg.nc
 mkdir -p data
 mv elev.0.25-deg.nc data/.
 gunzip HMA_cru_ts4.06.1901.2021.tmp.dat.nc.gz
